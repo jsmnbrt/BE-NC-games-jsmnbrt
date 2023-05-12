0c76ac7bc8e31779f049a7c694a29f11921e75bc
Jasmine Bertie <jasminebertie@Jasmines-MacBook-Pro.local>
2023-05-12 11:13:23 +0100

    completes ticket 4 for /api/review/:review_id

--- controllers/index.js
+++ controllers/index.js
@@ -1,4 +1,6 @@
 const { fetchCategories } = require('../models/index')
+const { selectReviewByID } = require('../models/index')
+const { checkReviewIDExists } = require('../models/index')
 
 exports.getCategories = (req, res, next) => {
     fetchCategories().then((categories) => {
@@ -7,3 +9,13 @@ exports.getCategories = (req, res, next) => {
         next(err)
     } )
 }
+
+exports.getReviewID = (req, res, next) => {
+    const { review_id } = req.params 
+    selectReviewByID(review_id).then((review) => {
+        res.status(200).send({ review: review })
+    }).catch((err) => {
+        next(err)
+    })
+}
+
--- models/index.js
+++ models/index.js
@@ -1,4 +1,7 @@
 const db = require('../db/connection')
+const connection = require('../db/connection')
+// const { checkReviewIDExists } = require('../utils')
+
 
 exports.fetchCategories = () => {
     return db.query(`SELECT * FROM categories;`).then((res) => {
@@ -6,9 +9,49 @@ exports.fetchCategories = () => {
     })
 }
 
-
-// exports.fetchCategories = () => {
-//     return db.query(`SELECT * FROM categories;`).then(( {rows}) => {
-//         return rows
+// exports.fetchReviews = (review_id) => {
+//     return db.query(`SELECT * FROM reviews WHERE review_id = $1;`, 
+//     [review_id]).then((res) => {
+//         return res.rows[0]
 //     })
 // }
+
+exports.selectReviewByID = (reviewID) => {
+    return db.query(`
+    SELECT * FROM reviews WHERE review_id = $1
+    `, [reviewID]).then(({rows}) => {
+        console.log(rows[0])
+        // if (rows.length === 0) {
+        //     return Promise.reject({ status: 404, msg: 'Not Found'})
+        // }
+        return rows [0]
+    })
+}
+
+
+// exports.selectReviewByID = (sort_by = 'review_id', review) => {
+//     const validSortQuery = ['review_id'];
+//     if (!validSortQuery.includes(sort_by)) {
+//         return Promise.reject({ status: 400, msg: 'invalid sort query'})
+//     }
+//     const queryStr = `
+//     SELECT reviews.*, review_id FROM reviews
+//     JOIN reviews ON reviews.review_id = reviews.review_id
+//     WHERE review_id = $1
+//     ORDER BY ${sort_by};`;
+
+//     return connection.query(queryStr, [review]).then((result) => {
+//         return result.rows;
+//     })
+
+// }
+
+
+// exports.selectReviewByID = (review_id)
+// return checkReviewIDExists(review)
+// .then(() => {
+//     return connection.query(queryStr, queryValues);
+// })
+// .then((result) => {
+//     return result.rows
+// })
